4a907ef2e6d4faa4d1e9473b4fff0c4226aba42b
Md. Shanto <md.si.shanto001@gmail.com>
2026-04-07 17:13:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4063414501-4b962",
+  "appId": "1:291352945127:web:be90f9e0c0b4ab3cf79d42",
+  "apiKey": "AIzaSyDvpt-Z-n-hlVtw9CbfMdRxeaJQNIqXlyU",
+  "authDomain": "studio-4063414501-4b962.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "291352945127"
+};
